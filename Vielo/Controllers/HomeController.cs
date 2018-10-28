1dd55497fa4fcb55ecf107c52508a5b6856e87c9
Pramod <thePramodGitHub>
2018-10-28 22:31:10 +0530

    changed description to descriptions in about action in home controller

--- Vielo/Controllers/HomeController.cs
+++ Vielo/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Vielo.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application descriptions page.";
 
             return View();
         }
